421e91699cec5e3c54e8b12a77fe1e5ec6ab9df3
Juliano Atanazio <juliano777@gmail.com>
2025-10-01 12:14:19 -0300

    .

--- whpg.sh
+++ whpg.sh
@@ -183,6 +183,15 @@ export MANPATH="\${WHPG_HOME}/man:\${MANPATH}"
 # PostgreSQL data directory
 export PGDATA='/var/local/whpg/data'
 
+#
+export PGPORT=5432
+
+#
+export PGUSER=gpadmin
+
+#
+export PGDATABASE=gpadmin
+
 # Unset variables
 unset WHPG_HOME PGBIN
 EOF
@@ -191,6 +200,62 @@ EOF
 # 
 echo "source /usr/local/whpg/greenplum_path.sh" >> ~/.bash_profile
 echo "source ~/.whpg_vars" >> ~/.bash_profile
+
+# 
+cat << EOF > ~/hostfile_gpinitsystem
+sdw1
+sdw2
+sdw3
+EOF
+
+# 
+cat << EOF > ~/gpinitsystem_config
+# #####################################################################
+# CONFIGURAÇÃO GERAL
+# #####################################################################
+
+# O nome do seu sistema de banco de dados (o nome do cluster).
+ARRAY_NAME="whcluster_acme"
+
+# O nome do banco de dados inicial a ser criado (geralmente 'postgres' ou 'template1').
+DATABASE_NAME="${PGDATABASE}"
+
+# A porta base para as instâncias de segmento.
+# A porta do Master será BASE_PORT + 1. Os segmentos usarão as portas seguintes.
+BASE_PORT=40000
+
+# O ID do Master. Use 0 para sistemas de teste.
+MASTER_ARRAY_HOST=0
+MASTER_PORT=5432 # A porta do Master no seu host (pode ser a 5432 padrão ou outra)
+
+# Prefixo para os diretórios de dados. Cada segmento terá um diretório
+# com este prefixo seguido de um número. Ex: /data/gpseg0, /data/gpseg1
+SEG_PREFIX=gpseg
+
+# #####################################################################
+# CONFIGURAÇÃO DE CAMINHOS
+# #####################################################################
+
+# Diretórios de dados do Master (coloque em um disco/volume persistente)
+# Exemplo: /home/gpadmin/gpdata/master
+MASTER_DIRECTORY=${PGDATA}
+
+# Diretórios de dados dos Segmentos. Deve ser um caminho *existente* em cada host
+# listado no hostfile. Para um VM, é um caminho único.
+# ATENÇÃO: Se usar múltiplos segmentos virtuais, você precisará de múltiplos diretórios.
+# Exemplo: DATA_DIRECTORY=/home/seu_usuario/gpdata/segmento1 /home/seu_usuario/gpdata/segmento2
+# Se você tiver 4 segmentos no hostfile, precisará de 4 caminhos aqui.
+DATA_DIRECTORY=/home/seu_usuario/gpdata/seg1 /home/seu_usuario/gpdata/seg2 /home/seu_usuario/gpdata/seg3 /home/seu_usuario/gpdata/seg4
+
+# #####################################################################
+# CONFIGURAÇÃO DE REDE (Recomendado para ambientes de teste)
+# #####################################################################
+
+# Tamanho do segmento (o número de segmentos primários no seu cluster).
+# Deve ser igual ao número de linhas no seu hostfile_gpinitsystem.
+MACHINE_SEGMENTS=3 # (Neste exemplo, assumindo 4 linhas no hostfile)
+EOF
+
 
 
 
